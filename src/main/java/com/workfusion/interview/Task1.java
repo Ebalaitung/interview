783fce32169a9c02e7e91251b0ef7e9b5a5c4ab3
akushunin <akushunin@workfusion.com>
2021-04-08 12:52:53 +0200

    initial commit // divide task

---
+++ src/main/java/com/workfusion/interview/Task1.java
@@ -0,0 +1,22 @@
+
+package com.workfusion.interview;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * <p>
+ * The digits are stored such that the most significant digit is at the head of the list,
+ * and each element in the array contain a single digit.
+ * <p>
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ */
+public class Task1 {
+
+    public int divide(int a, int b){
+        return 0;
+    }
+
+}
---
+++ src/test/java/com/workfusion/interview/Task1Test.java
@@ -0,0 +1,17 @@
+package com.workfusion.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Task1Test {
+
+    private Task1 task = new Task1();
+
+    @Test
+    void shouldPerformDivideOperation() {
+       assertEquals(3, task.divide(10, 3));
+    }
+
+
+}
