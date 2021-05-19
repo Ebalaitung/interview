2ddc4b93c39889861784ebd5a433bc415991b97e
Aliaksei <KAA_mmf@tut.by>
2021-05-19 15:09:45 +0300

    Update Task1.java

--- src/main/java/com/workfusion/interview/Task1.java
+++ src/main/java/com/workfusion/interview/Task1.java
@@ -6,12 +6,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 /**
- * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
- * <p>
- * The digits are stored such that the most significant digit is at the head of the list,
- * and each element in the array contain a single digit.
- * <p>
- * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ * Implement integer division without using /, % and math package
  */
 public class Task1 {
 
