75d4838dc755a04aafe038688ba568150719d00c
Aliaksei <KAA_mmf@tut.by>
2022-06-24 12:49:54 +0200

    Update Task1.java

--- src/main/java/com/workfusion/interview/Task1.java
+++ src/main/java/com/workfusion/interview/Task1.java
@@ -1,10 +1,6 @@
 
 package com.workfusion.interview;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 /**
  * Implement integer division without using /, % and math package
  */
